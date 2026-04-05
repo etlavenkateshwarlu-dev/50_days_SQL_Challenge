59c0c8331972fedd5f9a3672bea940872b5f44da
etlavenkateshwarlu-dev <etla.venkateshwarlu@gmail.com>
2026-04-05 08:52:24 +0530

    Da6 task compleetd

--- Queries/Day6.sql
+++ Queries/Day6.sql
@@ -10,3 +10,20 @@ select  min(salary) minumum_salry ,
 max(salary) minumum_salry,
 avg(salary) avg_salry
 from cleaned_salaries where salary>0;
+
+alter table cleaned_salaries 
+alter column salary TYPE INTEGER
+using salary::INTEGER;
+
+alter table cleaned_salaries add column in_outlier  INTEGER;
+
+select  min(salary) minumum_salry ,
+max(salary) minumum_salry,
+avg(salary) avg_salry
+from cleaned_salaries where salary>0;
+
+update cleaned_salaries set in_outlier = case when salary <20000 or salary >200000 
+											then 1 
+											else 0
+											end;
+                                             
