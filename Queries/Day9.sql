f786c68ecb408a55dca0a52c1a56f7952597339b
etlavenkateshwarlu-dev <etla.venkateshwarlu@gmail.com>
2026-04-08 08:49:52 +0530

    Day9 task completed

--- Queries/Day9.sql
+++ Queries/Day9.sql
@@ -1,2 +1,30 @@
---Day 9 : Applay joins 
---Fin
+--Day9 applay joins 
+--Find the employees who have valid deparment 
+select 
+e.emp_id,e.emp_name,d.dept_name 
+from cleaned_employees e
+inner join cleaned_departments d
+on e.dept_id =d.dept_id ;
+
+-- Find All employes even with out department 
+
+select 
+e.emp_id,e.emp_name,d.dept_name 
+from cleaned_employees e
+left join cleaned_departments d
+on e.dept_id =d.dept_id ;
+
+-- List employe with out department
+select 
+e.emp_id,e.emp_name,d.dept_name 
+from cleaned_employees e
+left join cleaned_departments d
+on e.dept_id =d.dept_id where d.dept_id is null;
+
+
+select 
+e.emp_id,e.emp_name,s.salary  
+from cleaned_employees e
+ join cleaned_salaries s
+on e.emp_id  =s.emp_id  ;
+
